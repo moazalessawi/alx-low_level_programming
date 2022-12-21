a8adaa002cf92a5e72f50239d8b9fb8746ba41e8
moazalessawi <moazalessawi@Moaz-AlEssawi.localdomain>
2022-12-21 02:40:13 +0200

    M

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,10 @@
- #include "main.h"
+#include "main.h"
 /**
-*  reset_to_98 - takes the pointer and updates the value
-*  @n: int to check
-*  Owned By Bwave/Bright
-*  Return: 0 is success
-*/
-
+ * reset_to_98 -function that takes a pointer to an int as parameter and
+ * update the valur it to 98.
+ *
+ * @n: this is the empty
+ */
 void reset_to_98(int *n)
 {
 	*n = 98;
